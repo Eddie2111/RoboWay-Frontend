be0010cd6fee940057bdb6f73854c5e5f677cace
ASM Tareq Mahmood <asm.tareq.mahmood@g.bracu.ac.bd>
2023-01-16 20:42:51 +0600

    Update api.js

--- src/pages/components/backend/api.js
+++ src/pages/components/backend/api.js
@@ -4,7 +4,7 @@ export const Run = async()=>{
     await axios.get(Backend, {withCredentials: true, headers: {'Content-Type': 'application/json'} })
 }
 
-export const Backend = "https://roboway.onrender.com/";
+export const Backend = "https://red-termite-sock.cyclic.app/";
 
 // export const Backend = "http://localhost:3001/";
 
