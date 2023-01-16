c3ef7bf0684d66698213abc62dc633d9e63ca95b
ASM Tareq Mahmood <asm.tareq.mahmood@g.bracu.ac.bd>
2023-01-16 21:08:51 +0600

    Update api.js

--- src/pages/components/backend/api.js
+++ src/pages/components/backend/api.js
@@ -1,11 +1,8 @@
 import axios from "axios";
 
+export const Backend = process.env.REACT_APP_BACKEND;
+
 export const Run = async()=>{
     await axios.get(Backend, {withCredentials: true, headers: {'Content-Type': 'application/json'} })
 }
-
-export const Backend = "https://red-termite-sock.cyclic.app/";
-
 // export const Backend = "http://localhost:3001/";
-
-
